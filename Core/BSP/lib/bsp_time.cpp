88bf6d4ab1e8acf0663721974a133ae7abf6b8c4
cxnaive <1012033254@qq.com>
2022-04-02 19:10:12 +0800

    update bsp_time

--- Core/BSP/lib/bsp_time.cpp
+++ Core/BSP/lib/bsp_time.cpp
@@ -1,1 +1,5 @@
 #include <bsp_time.h>
+
+uint32_t BSP_sys_time_ms() {
+    return HAL_GetTick();
+}
--- Core/BSP/lib/bsp_time.h
+++ Core/BSP/lib/bsp_time.h
@@ -2,8 +2,6 @@
 #define _BSP_TIME_H
 #include <stdint.h>
 #include "main.h"
-uint32_t BSP_sys_time_ms(void) {
-    return HAL_GetTick();
-}
+uint32_t BSP_sys_time_ms();
 
 #endif
